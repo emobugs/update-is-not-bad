f50e25501c539911377989b2e2aea1cf3b2b9ab9
Emo <emotupev11@gmail.com>
2021-09-18 21:14:11 +0300

    feat: add animation to article element on click event

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,16 +1,27 @@
-import EventEmitter from "eventemitter3";
-import anime from "animejs";
+import EventEmitter from 'eventemitter3';
+import anime from 'animejs';
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+	static get events() {
+		return {
+			READY: 'ready',
+		};
+	}
 
-  constructor() {
-    super();
-    this.init();
-    this.emit(Application.events.READY);
-  }
-  init() {}
+	constructor() {
+		super();
+		this.init();
+		this.emit(Application.events.READY);
+	}
+	init() {
+		const article = document.querySelector('article');
+
+		article.addEventListener('click', () => {
+			anime({
+				targets: '.article',
+				translateX: 250,
+				direction: 'alternate',
+				easing: 'spring(1, 80, 10, 0)',
+			});
+		});
+	}
 }
